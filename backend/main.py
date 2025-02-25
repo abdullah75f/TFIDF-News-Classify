57bccb9db0acf0f48776e48e90a43e9e52a4a638
Abdullah.F <abdullah75farid@gmail.com>
2025-02-25 14:07:58 +0300

    model accuracy design

--- backend/main.py
+++ backend/main.py
@@ -38,10 +38,10 @@ def predict_category(news: NewsInput):
 
 @app.get("/evaluate/")
 def evaluate_model():
-    # Load the test data
-    df = pd.read_json("../ml_model/dataset/News_Category_Dataset_v3.json", lines=True)
-     # Get the absolute path of the dataset file
+    # Get the absolute path of the dataset file
     dataset_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../ml_model/dataset/News_Category_Dataset_v3.json")
+    # Load the test data
+    df = pd.read_json(dataset_path, lines=True)
     X = df['headline']
     y = df['category']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)

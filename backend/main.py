44c084ebc845257e60a7f2709584437946fdd6cb
Abdullah.F <abdullah75farid@gmail.com>
2025-02-25 20:17:39 +0300

    accuracy endpoint

--- backend/main.py
+++ backend/main.py
@@ -39,19 +39,28 @@ def predict_category(news: NewsInput):
 @app.get("/evaluate/")
 def evaluate_model():
     try:
+        # Load the model and vectorizer
+        model = joblib.load('model.pkl')
+        vectorizer = joblib.load('vectorizer.pkl')
+
         # Get the absolute path of the dataset file
-        dataset_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../dataset/News_Category_Dataset_v3.json")
+        dataset_path = 'TFIDFNewsClassify/ml_model/dataset/News_Category_Dataset_v3.json'
         
-        # Load the test data
+        # Load the dataset
         df = pd.read_json(dataset_path, lines=True)
         X = df['headline']
         y = df['category']
+
+        # Train-Test Split
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+        # Transform the test set using the trained vectorizer
         X_test_tfidf = vectorizer.transform(X_test)
 
         # Predict and calculate accuracy
         y_pred = model.predict(X_test_tfidf)
         accuracy = accuracy_score(y_test, y_pred)
+
         return {"accuracy": accuracy}
     except Exception as e:
         return {"error": f"Evaluation failed: {e}"}
